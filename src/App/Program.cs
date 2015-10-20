7d04015640b0a6525a988158db85c5d8650f71d1
Kristof Mattei <github@kristofmattei.be>
2015-10-20 18:54:28 +0200

    Initial commit

---
+++ src/App/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace App
+{
+    using System;
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            Console.WriteLine("All done");
+            Console.ReadLine();
+        }
+    }
+}
